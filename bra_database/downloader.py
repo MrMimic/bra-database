a800ff09fa58052c86265911edb5244c487478f9
Emeric <emeric.dynomant@jellysmack.com>
2022-03-01 10:21:07 +0100

    :sparkles: Add PDF downloading facilities

--- bra_database/downloader.py
+++ bra_database/downloader.py
@@ -2,6 +2,8 @@
 """
 import logging
 from datetime import datetime
+import os
+import shutil
 
 import requests
 
@@ -12,10 +14,16 @@ class BraDownloader():
     """Download PDF files.
     """
 
-    def __init__(self, logger: logging.Logger = None):
+    def __init__(self, pdf_path: str, logger: logging.Logger = None):
         """Initialize the class.
         """
         self.logger = logger or get_logger()
+        self.pdf_path = pdf_path
+        self.file_name = []
+
+        if not os.path.exists(self.pdf_path):
+            os.makedirs(self.pdf_path)
+        self.logger.info(f"Downloading data in folder: {self.pdf_path}")
 
     @staticmethod
     def _create_file_path(date: str = None) -> str:
@@ -33,6 +41,20 @@ class BraDownloader():
         """A JSON file contains the timestamps of the files to be downloaded.
         """
         json_file_path = self._create_file_path(date="20220228")
-        self.logger.info(f"Downloading {json_file_path}")
-        json_file = requests.get(json_file_path).json()
-        print(json_file)
+        self.logger.info(f"Téléchargement de {json_file_path}")
+        self.timestamps_bra = requests.get(json_file_path).json()
+        self.logger.info(f"Données de {len(json_file_path)} massifs reçus.")
+
+    def get_pdf_file(self) -> None:
+        """Download a PDF file.
+        """
+        for bra in self.timestamps_bra:
+            for time in bra['heures']:
+                file_name = f"{bra['massif']}.{time}.pdf"
+                self.file_name.append(file_name)
+                if file_name not in os.listdir(self.pdf_path):
+                    bra_url = f"https://donneespubliques.meteofrance.fr/donnees_libres/Pdf/BRA/BRA.{file_name}"
+                    self.logger.debug(f"Téléchargement de {bra_url}")
+                    response = requests.get(bra_url, stream=True)
+                    with open(os.path.join(self.pdf_path, file_name), 'wb') as out_file:
+                        shutil.copyfileobj(response.raw, out_file)
--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,5 +1,8 @@
 """Test covering the downloader class.
 """
+import os
+import shutil
+import tempfile
 import unittest
 
 from bra_database.downloader import BraDownloader
@@ -10,10 +13,26 @@ class DownloaderTests(unittest.TestCase):
     """
 
     def setUp(self) -> None:
-        self.downloader = BraDownloader()
+        self.tmp = tempfile.mkdtemp()
+        self.downloader = BraDownloader(self.tmp)
 
-    def test_downloader(self):
-        """Test the downloader class.
+    def tearDown(self) -> None:
+        # Remove the temp folder
+        shutil.rmtree(self.tmp)
+        pass
+
+
+    def test_download_json(self):
+        """Test the JSON downloader class.
         """
         self.assertIsInstance(self.downloader, BraDownloader)
         self.downloader.get_json_timestamp_file()
+        self.assertIsInstance(self.downloader.timestamps_bra, list)
+        self.assertTrue(len(self.downloader.timestamps_bra) > 1)
+
+    def test_download_pdf(self):
+        """Test the PDF downloader class.
+        """
+        self.downloader.get_json_timestamp_file()
+        self.downloader.get_pdf_file()
+        self.assertTrue(len(os.listdir(self.tmp)) > 1)

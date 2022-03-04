9054a8109c33d67538b4d0f7eabcacf41975b813
Emeric <emeric.dynomant@jellysmack.com>
2022-03-04 15:24:51 +0100

    :green_heart: delete sdk bin file if already present

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
+if [ -f ${HOME}/google-cloud-sdk ]; then
+    rm ${HOME}/google-cloud-sdk
+fi
 curl https://sdk.cloud.google.com | bash -s -- --disable-prompts > /dev/null
 export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
 gcloud --version

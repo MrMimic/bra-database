372110a15def031cc648bddd2adaa2ba0798f340
Emeric <emeric.dynomant@jellysmack.com>
2022-03-04 15:48:39 +0100

    :construction_worker: remove gcloud cache

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@ set -ex
 
 ls -alstrh
 echo $PWD
+echo $HOME
+
+if [ -f /home/travis/google-cloud-sdk ]; then
+    rm /home/travis/google-cloud-sdk
+fi
 
 curl https://sdk.cloud.google.com > install.sh
 bash install.sh --disable-prompts

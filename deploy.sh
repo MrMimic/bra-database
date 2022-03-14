fb9dae2b25d15096b51d63e466c45f518da87dc5
Emeric <emeric.dynomant@jellysmack.com>
2022-03-14 18:37:23 +0100

    :bug: write auth key in the pwd folder

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,9 @@ jq --version
 
 # Authenticate gcloud
 
-jq -n "{type:\"$GCLOUD_TYPE\",project_id:\"$GCLOUD_PROJECT_ID\",private_key_id:\"$GCLOUD_PRIVATE_KEY_ID\",private_key:\"$GCLOUD_PRIVATE_KEY\",client_email:\"$GCLOUD_CLIENT_EMAIL\",client_id:\"$GCLOUD_CLIENT_ID\",auth_uri:\"$GCLOUD_AUTH_URI\",token_uri:\"$GCLOUD_TOKEN_URI\",auth_provider_x509_cert_url:\"$GCLOUD_AUTH_PROVIDER_X509\",client_x509_cert_url:\"$GCLOUD_CLIENT_X509\"}" > $PWD/gcloud_auth.json
+jq -n "{type:\"$GCLOUD_TYPE\",project_id:\"$GCLOUD_PROJECT_ID\",private_key_id:\"$GCLOUD_PRIVATE_KEY_ID\",private_key:\"$GCLOUD_PRIVATE_KEY\",client_email:\"$GCLOUD_CLIENT_EMAIL\",client_id:\"$GCLOUD_CLIENT_ID\",auth_uri:\"$GCLOUD_AUTH_URI\",token_uri:\"$GCLOUD_TOKEN_URI\",auth_provider_x509_cert_url:\"$GCLOUD_AUTH_PROVIDER_X509\",client_x509_cert_url:\"$GCLOUD_CLIENT_X509\"}" > gcloud_auth.json
 
-gcloud auth login --no-browser --brief --cred-file=$PWD/gcloud_auth.json
+gcloud auth login --no-browser --brief --cred-file=gcloud_auth.json
 
 docker tag bra/backend gcr.io/data-baguette/bra-backend
 docker push gcr.io/data-baguette/bra-backend

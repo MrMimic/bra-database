8f171d070b8d10c6d66d256788bd3811b2de25cd
Emeric <emeric.dynomant@jellysmack.com>
2022-03-14 08:28:24 +0100

    :passport_control: authenticate travis to gcloud

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 set -ex
 
-ls -alstrh
-echo $PWD
-echo $HOME
-
+# Remove the existing gcloud SDK and install last verison
 if [ -f /home/travis/google-cloud-sdk ]; then
     rm /home/travis/google-cloud-sdk
 fi
-
 curl https://sdk.cloud.google.com > install.sh
 bash install.sh --disable-prompts
 
+# Check utilities
 gcloud --version
+jq --version
+
+# Authenticate gcloud
+echo $GCLOUD_AUTH | jq > $PWD/gcloud_auth.json
+gcloud auth login --no-browser --brief --cred-file=$PWD/gcloud_auth.json
+
+docker tag bra/backend gcr.io/data-baguette/bra-backend
+docker push gcr.io/data-baguette/bra-backend
